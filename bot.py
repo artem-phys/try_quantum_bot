8839e2965e5d6f0674cde916459b30d6617787be
artem-phys <artemkuzmichev.official@gmail.com>
2023-08-18 06:51:42 +0300

    add new

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import telebot
 
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit.visualization import plot_bloch_vector
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
@@ -10,12 +11,13 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Здравствуйте! Я try_quantum_bot. С моей помощью можно познакомиться с миром квантовых вычислений")
+    bot.reply_to(message, "Привет! Я try_quantum_bot. С моей помощью можно познакомиться с миром квантовых вычислений")
 
 
+# Отображение состояния на сфере Блоха
 @bot.message_handler(commands=['bloch_sphere'])
 def bloch_sphere_handler(message):
-    text = "Наберите в сообщении состояние для отображения на сфере Блоха"
+    text = "Набери в сообщении состояние для отображения на сфере Блоха"
     sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
     bot.register_next_step_handler(sent_msg, display_bloch_sphere_handler)
 
@@ -27,7 +29,32 @@ def display_bloch_sphere_handler(message):
 
     result.savefig('bloch_state.png', dpi=300, bbox_inches="tight")
     bot.send_photo(message.chat.id, photo=open('bloch_state.png', 'rb'),
-                   caption="Ваше состояние")
+                   caption="Сфера Блоха")
+
+
+# Отображение квантовой цепочки из QASM сода
+    @bot.message_handler(commands=['qasm_code'])
+    def qasm_handler(message):
+        text = "Введи код на QASM, и я покажу твою цепочку"
+        sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+        bot.register_next_step_handler(sent_msg, qasm_code_handler)
+
+    def qasm_code_handler(message):
+        text = "Вот что получилось"
+
+        # Build a quantum circuit
+        circuit = QuantumCircuit(3, 3)
+
+        circuit.x(1)
+        circuit.h(range(3))
+        circuit.cx(0, 1)
+        circuit.measure(range(3), range(3));
+
+        qc_fig = circuit.draw('mpl')
+
+        qc_fig.savefig('qc.png', dpi=300, bbox_inches="tight")
+        bot.send_photo(message.chat.id, photo=open('qc.png', 'rb'),
+                       caption="Квантовая цепочка")
 
 
 @bot.message_handler(func=lambda msg: True)
